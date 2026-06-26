1b000adcdccec863ebb5093ea3952a85dd2af798
murillo <erickceballo20@gmail.com>
2026-06-26 16:02:51 -0400

    el tio

---
+++ caso.py
@@ -0,0 +1,12 @@
+Calculadora de IVA: Crea un programa que declare una variable con el precio de un
+producto (un número flotante) y otra con el porcentaje de IVA (por ejemplo, 21). El
+programa debe calcular el monto del IVA y el precio final total, imprimiendo un
+desglose claro en la consola.
+
+manzana= 50.33
+piña= 70.5
+int= manzana
+float= piña 
+monto_iva= manzana * (piña / 100)
+print(monto_iva)
+precio_final= (manzana + monto_iva)
