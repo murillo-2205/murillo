7c996b1318a4ad53da02045c6753feaef482261a
murillo <erickceballo20@gmail.com>
2026-06-26 21:10:46 -0400

    murillo

---
+++ calculadora promedio simple.py
@@ -0,0 +1,10 @@
+mensaje= 'resultado de murillo'
+print(mensaje)
+examen_math=80
+examen_geo=90
+examen_ing=100
+int(examen_math)
+int(examen_geo)
+int(examen_ing)
+promedio=(examen_math + examen_geo + examen_ing)/3
+print(promedio)
