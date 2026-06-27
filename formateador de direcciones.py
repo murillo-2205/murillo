4bd4b061ef75bbb38a7a0789c2072a268a75d8b4
murillo <erickceballo20@gmail.com>
2026-06-26 20:48:10 -0400

    murillo

---
+++ formateador de direcciones.py
@@ -0,0 +1,12 @@
+
+
+calle= 'calle a'
+num= '21'
+ciudad= 'san francisco de macoris'
+codigo= '31000'
+
+mensaje= 'usted a insertado los datos correctamente'
+print(mensaje)
+resultado= f"{calle}, #{num}, {ciudad}, {codigo}"
+print(resultado)
+
