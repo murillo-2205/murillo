4d845837c6d94f9336fc7cf1c59b1d4800ce3524
murillo <erickceballo20@gmail.com>
2026-06-26 20:14:45 -0400

    murillo

---
+++ intercambio de valores.py
@@ -0,0 +1,18 @@
+
+
+
+a= 5
+b=10
+mensaje= 'vamos a comenzar'
+print(mensaje)
+int(a)
+int(b)
+proceso= (a + b -5)
+print(proceso)
+int (a)
+int (b)
+print(a - b + 10)
+mensaje= 'resultado excelente'
+print(mensaje)
+
+
