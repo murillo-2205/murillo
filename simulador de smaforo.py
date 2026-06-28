38c32e92622416f3424e1c3563b1ef52c2347a2e
murillo <erickceballo20@gmail.com>
2026-06-27 21:15:46 -0400

    murillo

---
+++ simulador de smaforo.py
@@ -0,0 +1,8 @@
+
+color_semaforo= input('ingrese el color')
+if color_semaforo == 'rojo':
+    print('detengase')
+elif color_semaforo =='amarillo':
+    print('frene de espacio')
+elif color_semaforo =='verde':
+    print('avanze')
