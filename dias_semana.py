94051fb7a5316b3dd1d03127511912f1a9accdeb
murillo <erickceballo20@gmail.com>
2026-07-02 14:12:48 -0400

    murillo

---
+++ dias_semana.py
@@ -0,0 +1,19 @@
+numero = int(input("Ingrese un número del 1 al 7: "))
+
+match numero:
+    case 1:
+        print("Lunes")
+    case 2:
+        print("Martes")
+    case 3:
+        print("Miércoles")
+    case 4:
+        print("Jueves")
+    case 5:
+        print("Viernes")
+    case 6:
+        print("Sábado")
+    case 7:
+        print("Domingo")
+    case _:
+        print("Número fuera de rango.")
