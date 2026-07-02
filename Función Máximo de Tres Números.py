98337d237126c3e4adb4f32c0248fc11bfca800c
murillo <erickceballo20@gmail.com>
2026-07-02 14:29:49 -0400

    murillo

---
+++ Función Máximo de Tres Números.py
@@ -0,0 +1,15 @@
+def mayor_de_tres(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+num3 = float(input("Ingrese el tercer número: "))
+
+resultado = mayor_de_tres(num1, num2, num3)
+
+print("El número mayor es:", resultado)
