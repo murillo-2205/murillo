df19054b212da27124822e98aa7bfa4b9c0b8b99
murillo <erickceballo20@gmail.com>
2026-07-17 00:20:30 -0400

    murillo

---
+++ for while 4.py
@@ -0,0 +1,6 @@
+#Escribe un programa que calcule la suma de todos
+#los números enteros del 1 al 100 utilizando un bucle for.
+
+for murillo in range(0,100):
+    murillo= murillo +1
+    print('numero: {0} + {1} = {2}'.format(murillo, murillo, (murillo + murillo)))
