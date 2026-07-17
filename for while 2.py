b0163019bfba53231d4c8277595729f4f3723279
murillo <erickceballo20@gmail.com>
2026-07-16 23:59:16 -0400

    murillo

---
+++ for while 2.py
@@ -0,0 +1,5 @@
+#Escribe un programa que imprima los números pares
+#entre 1 y 20 (inclusive) utilizando un bucle for.
+for murillo in range(0,20):
+    print('numero: {0}'.format(murillo))
+print('el 20 es un tio')
