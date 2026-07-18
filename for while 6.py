f388ae91757d3956c9983c41009369c042e80e65
murillo <erickceballo20@gmail.com>
2026-07-17 23:16:32 -0400

    murillo

---
+++ for while 6.py
@@ -0,0 +1,6 @@
+#Escribe un programa que imprima la tabla de multiplicar de un número 
+#dado por el usuario, desde el 1 hasta el 10, utilizando un bucle for.
+branyu= int(input('ingrese un numero positivo'))
+for murillo in range(1,11):
+    print('valor: {0} x {1} = {2}'.format(branyu, murillo, (branyu * murillo)))
+    
