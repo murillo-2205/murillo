28cb9e41dc7cf752d19fa256d241c32064513af6
murillo <erickceballo20@gmail.com>
2026-07-17 23:22:00 -0400

    murillo

---
+++ while 7.py
@@ -0,0 +1,10 @@
+#Escribe un programa que pida al usuario un número entero  positivo y  luego imprima
+#todos los números impares desde 1 hasta el número ingresado utilizando un bucle while.
+
+numero = int(input("Ingrese un número entero positivo: "))
+
+i = 1
+
+while i <= numero:
+    print(i)
+    i += 2
